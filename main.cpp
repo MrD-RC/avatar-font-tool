80d2cf1ae627c16099d649f2e0ab5e7decec48fc
Darren Lines <darren@darrenlines.uk>
2022-09-11 07:12:30 +0100

    Added licence information

--- main.cpp
+++ main.cpp
@@ -1,3 +1,21 @@
+/**
+ * This file is part of Avatar Font Tool.
+ * Written by Darren Lines (Mr. D RC)
+ *
+ * Avatar Font Tool is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version. Please keep this header in the files.
+ *
+ * Avatar Font Tool is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Avatar Font Tool.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include <iostream>
 #include <vector>
 #include <filesystem>
